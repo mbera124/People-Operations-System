104870e746c227ef7e6238d796ba5f355a48fa77
mbera124 <josephmbera124@gmail.com>
2024-01-06 17:59:57 +0300

    init fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
+import "../css/index.css";
 import "antd/dist/reset.css";
 import "bootstrap/dist/css/bootstrap.css";
 import { store } from "./redux/store";
